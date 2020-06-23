22b1bb7766beca07e43487a35822963fe48f20cf
Jeremy Berman <jerber@sas.upenn.edu>
2020-06-23 01:41:31 -0400

    added async without firestore

--- app/Models/TestUser.py
+++ app/Models/TestUser.py
@@ -2,6 +2,7 @@ from magicdb.Models import MagicModel
 from pydantic import BaseModel
 
 
+# made BaseModel so that this will work without connecting your DB
 class TestUser(MagicModel):
     name: str
     age: int
--- app/Routes/test_async.py
+++ app/Routes/test_async.py
@@ -19,7 +19,13 @@ def sleep_for_five(secs: float, test_user: TestUser):
     print("ended sleep for test user", test_user)
 
 
-@router.post("/test_async")
+@router.post("/test_async_with_firestore")
 def test_async(*, secs: float = 5, test_user: TestUser):
     task_id = sleep_for_five(secs, test_user)
+    return {"task_id": task_id}
+
+
+@router.post("/test_async_without_firestore")
+def test_async(*, secs: float = 5):
+    task_id = sleep_for_five(secs, TestUser(name="Jon", age=2))
     return {"task_id": task_id}
