31d625a98d91c6f559e193398277560f3639cce2
Jeremy Berman <jerber@sas.upenn.edu>
2020-06-23 00:49:57 -0400

    smart search for service account json

--- app/magic/config.py
+++ app/magic/config.py
@@ -2,16 +2,29 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+SERVICE_ACCOUNT_NAME = "my-service-account.json"
+
+
+def get_service_account_path():
+    """Tries to get the google account credentials service account json in either
+    the base directory or in the magic directory"""
+    curr_dir_path = Path(__file__).parent.absolute()
+    this_dir_service_path = curr_dir_path / SERVICE_ACCOUNT_NAME
+
+    return (
+        this_dir_service_path
+        if this_dir_service_path.exists()
+        else Path.cwd() / SERVICE_ACCOUNT_NAME
+    )
+
+
 env_path = os.path.join(os.getcwd(), ".env")
 if os.path.exists(env_path):
     load_dotenv(env_path)
 
+# check either in the cwd or in this dir for the service account
 
-curr_dir_path = Path(__file__).parent.absolute()
-service_account_name = "my-service-account.json"
-
-service_account_path = Path(curr_dir_path) / service_account_name
-
+service_account_path = get_service_account_path()
 if service_account_path.exists():
     # will this work on windows?
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(
