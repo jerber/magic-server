e0830dd939d1b10374241298b4cfc7ec5b70657a
Jeremy Berman <jerber@sas.upenn.edu>
2020-07-02 14:23:29 -0400

    added sending logs to logdna via yaml

--- app/Routes/test_auth.py
+++ app/Routes/test_auth.py
@@ -3,4 +3,5 @@ from app.magic import router, CurrentUser, GET_USER
 
 @router.get("/get_current_user", response_model=CurrentUser)
 def get_current_user(current_user: CurrentUser = GET_USER):
+    print(current_user)
     return current_user
--- app/magic/__init__.py
+++ app/magic/__init__.py
@@ -53,6 +53,7 @@ async def add_process_time_header(request: Request, call_next):
 
 @app.get("/")
 def read_root():
+    print("hello world!")
     return {"Hello": "World", "cwd": Path.cwd(), "dir": os.listdir()}
 
 
