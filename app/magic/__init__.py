e89ba3689a114fb9c4e78be56b92152f35fcbcc8
Jeremy Berman <jerber@sas.upenn.edu>
2020-07-02 14:56:54 -0400

    warmup now working

--- app/magic/__init__.py
+++ app/magic/__init__.py
@@ -52,7 +52,16 @@ async def add_process_time_header(request: Request, call_next):
 
 
 @app.get("/")
-def read_root():
+def read_root(request: Request):
+    aws_event = request.scope.get("aws.event")
+    print("aws event", aws_event)
+    if aws_event and aws_event.get("source") in [
+        "aws.events",
+        "serverless-plugin-warmup",
+    ]:
+        print("Lambda is warm!")
+        return {}
+
     print("hello world!")
     return {"Hello": "World", "cwd": Path.cwd(), "dir": os.listdir()}
 
