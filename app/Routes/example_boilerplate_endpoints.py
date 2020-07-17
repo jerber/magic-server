d2a42757e59d04d957a1962239cc598a5e40eb63
Jeremy Berman <jerber@sas.upenn.edu>
2020-07-16 22:20:41 -0400

    changing conc

--- app/Routes/example_boilerplate_endpoints.py
+++ app/Routes/example_boilerplate_endpoints.py
@@ -92,13 +92,19 @@ def test_file(file: UploadFile = File(...)):
 
 """Sending to segment example and test."""
 
-
+from app.magic.Services.Segment import analytics
 from app.magic.Services import Segment
 
 
 @router.post("/test_segment", tags=["boilerplate"])
 def test_segment(id: str, action, body: dict):
     Segment.track(id, action, body)
+    return "done!"
+
+
+@router.post("/test_segment_without_background", tags=["boilerplate"])
+def test_segment(id: str, action, body: dict):
+    analytics.track(id, action, body)
     return "done!"
 
 
